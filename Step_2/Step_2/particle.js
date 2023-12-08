13e24d1dc5cec546c84bf41e19c58f29eef42342
memil7 <147472943+memil7@users.noreply.github.com>
2023-12-08 23:25:07 +0900

    Step_2_2
    tchoice의 변화

---
+++ Step_2/Step_2/particle.js
@@ -0,0 +1,47 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+// Simple Particle System
+
+// A simple Particle class
+
+class Particle {
+  constructor(x, y) {
+    this.position = createVector(x, y);
+    this.velocity = createVector(random(-1, 1), random(-1, 0));
+    this.acceleration = createVector(0, 0);
+    this.lifespan = 255.0;
+    this.c = color(0);
+  }
+
+  run() {
+    this.update();
+    this.show();
+  }
+
+  applyForce(f) {
+    this.acceleration.add(f);
+  }
+
+  // Method to update position
+  update() {
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.lifespan -= 2;
+    this.acceleration.mult(0);
+  }
+
+  // Method to display
+  show() {
+    stroke(this.c, this.lifespan);
+    strokeWeight(2);
+    fill(this.c, this.lifespan);
+    circle(this.position.x, this.position.y, 1);
+  }
+
+  // Is the particle still useful?
+  isDead() {
+    return this.lifespan < 0.0;
+  }
+}
